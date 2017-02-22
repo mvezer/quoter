296e9077d96f70360f6c77f01f79638fa170ff23
mvezer <vezer.m@gmail.com>
2017-02-22 15:11:20 +0100

    externalizing json validation

--- src/lib/controller/QuotePostController.ts
+++ src/lib/controller/QuotePostController.ts
@@ -1,14 +1,11 @@
-import { QuoteSchema } from './../model/schema/QuoteSchema';
+import * as Schema from './../model/Schema';
 import { IQuotePostControllerConfig } from "./IController";
 import { Request, IReply, IRouteConfiguration } from "hapi";
 import { ARouteController } from "./ARouteController";
-import * as Joi from "joi";
+import * as ValidationUtil from "../util/ValidationUtil";
 
 export default class QuotePostController extends ARouteController {
-
-
     protected config: IQuotePostControllerConfig;
-
     constructor(config: IQuotePostControllerConfig) {
         super(config);
     }
@@ -16,7 +13,7 @@ export default class QuotePostController extends ARouteController {
     routeHandler = async (request: Request, reply: IReply): Promise<void> => {
         return new Promise<void>(async (resolve, reject) => {
             let value: any;
-            this.validate(request.payload, QuoteSchema)
+            ValidationUtil.validate(request.payload, Schema.QuoteSchema)
                 .then(async (payload) => {
                     await this.config.messageBrokerService.publish(this.config.routingKey, payload);
                     this.replyOk(reply);
@@ -26,18 +23,6 @@ export default class QuotePostController extends ARouteController {
                     this.replyError(reply, error);
                     error.isJoi ? resolve() : reject(error);  // not rejecting validation errors
                 })
-        });
-    }
-
-    async validate(buffer: any, schema: Joi.Schema): Promise<any> {
-        return new Promise<Buffer>((resolve, reject) => {
-            Joi.validate<any, void>(buffer, schema, (err: Joi.ValidationError, value: any) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(value);
-                }
-            })
         });
     }
 
---
+++ src/lib/util/ValidationUtil.ts
@@ -0,0 +1,14 @@
+import * as Joi from "joi";
+
+// Promisify the Joi.validate function
+export async function validate(buffer: any, schema: Joi.Schema): Promise<any> {
+    return new Promise<Buffer>((resolve, reject) => {
+        Joi.validate<any, void>(buffer, schema, (err: Joi.ValidationError, value: any) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(value);
+            }
+        })
+    });
+}
